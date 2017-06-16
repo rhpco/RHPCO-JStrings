198f86ae033584b479c5fd242581f16572bfa251
rhpco <rakkapriccio@gmail.com>
2017-06-16 15:21:29 +0200

    First commit

---
+++ RHPCO-JStrings.js
@@ -0,0 +1,36 @@
+/**
+ * RHPCO-JStrings - Extracts all Literal from an input javascript file
+ * rakkapriccio@gmail.com
+ *
+ * Usage:
+ * $ node RHPCO-JStrings.js input_file.js
+ */
+var fs = require('fs');
+var async = require('async');
+var esprima = require('esprima');
+var estraverse = require('estraverse');
+
+function listStrings(code){
+    var strings=[];
+    var ast = esprima.parse(code);
+    estraverse.traverse(ast, {
+        enter: function(node){
+            if (node.type === 'Literal'  ){
+                strings.push(node.value);
+            }
+        }
+    });
+    return strings;
+}
+
+if( typeof process.argv[2] != null ){
+    fs.readFile(process.argv[2], 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        results = listStrings(data);
+        results.forEach(function(s){
+            console.log(s);
+        })
+    });
+}
